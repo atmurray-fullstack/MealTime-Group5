24500a942c26cff6464b37607324c8dce6841b71
Andrew Murray <atmurray@bellsouth.net>
2020-06-04 12:14:53 -0400

    moved main.handlebars and made sure main page would render

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,13 +1,17 @@
 
 const sequelize = require('sequelize');
 const User =  require('../models/User')
-
+const path = require('path')
 module.exports = function(app){
     console.log('html-routes');
+
  app.get('/',function(req, res){
-     User.findAll()
-     .then(data=>{
-         res.json(data);
-     })
+    console.log(path.join(__dirname, '../views/index.handlebars'))
+
+    res.render(path.join(__dirname, '../views/index.handlebars'))
+    //  User.findAll()
+    //  .then(data=>{
+    //      res.json(data);
+    //  })
  })
 };
