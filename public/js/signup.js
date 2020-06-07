c02f8f86c3865e486b2e8bd340fb5804f7712671
Andrew Murray <atmurray@bellsouth.net>
2020-06-07 19:42:29 -0400

    added signup.handlebars and member.handlebars started writing events listeners for views

---
+++ public/js/signup.js
@@ -0,0 +1,42 @@
+
+$(document).ready(function () {
+
+
+    const form = $("#signup-form");
+    const firstName = $("#signup-firstName");
+    const lastName = $("#signup-lastName");
+    const address = $("#signup-address");
+    const email = $("#signup-email");
+    const password = $("#signup-password");
+
+
+    $("#signup-form").on("submit", (event) => {
+        event.preventDefault();
+        const user = {
+            firstName: firstName.val().trim(),
+            lastName: lastName.val().trim(),
+            address: address.val().trim(),
+            email: email.val().trim(),
+            password: password.val().trim()
+        };
+
+        if (!user.email || !user.password) {
+            return;
+        }
+
+
+        createNewUser(user.firstName, user.lastName, user.address, user.email, user.password)
+    })
+
+    function createNewUser(fName, lName, address, email, password) {
+
+        $.post("/api/createUser", {
+            first_name: fName,
+            last_name: lName,
+            userName: email,
+            passWord: password,
+            address: address
+        })
+    }
+
+});
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,3 +1,22 @@
-module.exports = function(app){
-    console.log('api-routes');
+const User = require("../models/User");
+const Orders = require("../models/orders");
+// const bcrypt = require("bcrypt");
+
+module.exports = function (app) {
+console.log("api-routes")
+
+app.post("/api/createUser", (req, res) => {
+    console.log(req.body);
+    const user = req.body;
+    
+    // User.create(user)
+   
+    res.json({});
+})
+
+
+
+
+
+
 };
