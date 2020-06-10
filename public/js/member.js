4df4256328fde1ca679dcd7f8048d3247a8b47c8
Christy <mianmianguo2020@gmail.com>
2020-06-10 01:09:12 -0400

    update

--- public/js/member.js
+++ public/js/member.js
@@ -50,21 +50,6 @@ function getCookie(cname) {
   };
 
 
-    // $("#submitInfor").on("click", function (event) {
-    //     event.preventDefault();
-    //     $.post('http://localhost:8080/api/submitMealPlan', { mealBudget: 4.56 }, function(data) {
-    //         // console.log(data)
-    //     })
-    //     const address = '';//will be able to get this information from DB
-    //     const userSeach = '';//will be getting from page
-    //     const mealBudget = ""//
-
-    //     // $.post('http://localhost:8080/api/submitMealPlan', { likes: burger }, function(data) {
-    //     //     // console.log(data)
-    //     // })
-    // });
-
-
 
 
 function handleRestaurantNameClick(element) {
