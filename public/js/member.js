6f8a3a14cd619aac0c9af5bd4b14e6d20bee234b
majo <maria.riofrio.mr@gmail.com>
2020-06-10 21:54:39 -0400

    current work

--- public/js/member.js
+++ public/js/member.js
@@ -16,11 +16,11 @@ $(document).ready(function () {
 
 
     $(".datepicker").datepicker({
-        // setDefaultDate: new Date(2000,01,31),
-        defaultDate: new Date(currYear - 20, 1, 31),
-        maxDate: new Date(currYear - 20, 12, 31),
-        yearRange: [1928, currYear - 20],
-        format: "yyyy/mm/dd"
+      // setDefaultDate: new Date(2000,01,31),
+      defaultDate: new Date(currYear - 20, 1, 31),
+      maxDate: new Date(currYear - 20, 12, 31),
+      yearRange: [1928, currYear - 20],
+      format: "yyyy/mm/dd"
     });
 
 });
