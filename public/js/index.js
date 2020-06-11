0814637844c4b22125ac7ad5c1522521e82661c7
Andrew Murray <atmurray@bellsouth.net>
2020-06-10 21:01:36 -0400

    added mealTimeUser object to member.js and created address cookie for logged in user

--- public/js/index.js
+++ public/js/index.js
@@ -45,8 +45,9 @@ $(document).ready(function () {
             .done((data => {
                 console.log(data);
                 if (data) {
- 
+                    console.log(data);
                     document.cookie = "mealTime-userName =" + data.userName + ";path=/"
+                    document.cookie = "mealTime-userAddress =" + data.address + ";path=/"
                     document.location.href = '/member'
                 } else {
                     alert("Incorrect Login")
