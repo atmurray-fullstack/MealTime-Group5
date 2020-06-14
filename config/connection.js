678de1545cda29fa6067a25b96d801e406b7d03a
majo <maria.riofrio.mr@gmail.com>
2020-06-14 17:05:54 -0400

    fixed member page

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,8 @@
 const Sequelize = require('sequelize');
 
-<<<<<<< HEAD
 const sequelize = new Sequelize('mealTime_db', 'root', 'Cl@u0218', {
-=======
-const sequelize = new Sequelize('mealTime_db', 'root', 'root', {
->>>>>>> 9f4cfcffa0b58802536b9c9216d031e1f46de2b0
     host: '127.0.0.1',
-    port: 3307,
+    port: 3306,
     dialect: 'mysql',
     pool: {
         max: 5,
