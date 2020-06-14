efff5e7bad68a26223fb0e4b676c5ccde52dcf54
majo <maria.riofrio.mr@gmail.com>
2020-06-14 16:20:05 -0400

    fixed responsive cards

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('mealTime_db', 'root', '', {
+const sequelize = new Sequelize('mealTime_db', 'root', 'Cl@u0218', {
     host: '127.0.0.1',
     port: 3306,
     dialect: 'mysql',
