2ed45a85e469ee56aed0c9976402b2f8f6608a22
Christy <mianmianguo2020@gmail.com>
2020-06-14 14:35:19 -0400

    update delete and local

--- public/js/member.js
+++ public/js/member.js
@@ -13,10 +13,10 @@ const keyWords = $("#key-words").val();
 var currYear = (new Date()).getFullYear();
 
 $(document).ready(function () {
-  $("form").submit((event)=> {
-      // alert("after form submit/ mianmian love peter")
-      alert(event.keys())
-      $(".orderList").append(`<div><i class="material-icons"></i>${date}</div>`)
+  $("form").submit((event) => {
+    // alert("after form submit/ mianmian love peter")
+    alert(event.keys())
+    $(".orderList").append(`<div><i class="material-icons"></i>${date}</div>`)
   })
   let shoppingList = JSON.parse(window.localStorage.getItem("shoppingList"))
   createItemList(shoppingList)
@@ -24,7 +24,7 @@ $(document).ready(function () {
 
   $(".button-collapse").sideNav();
   console.log(mealTimeCurrentUser);
-  
+
 
   $(".logOutButton").on("click", function (event) {
     deleteUser();
@@ -93,45 +93,60 @@ function handleRestaurantNameClick(element) {
 
 function createItemList(shoppingList) {
   const allItemLists = []
-  $('.orderedItem').empty()
-  Object.keys(shoppingList).forEach((element) => {
-    if (shoppingList[element].length != 0) {
-      $(".orderedItem").append(`<div>${element} : </div>`)
-      for (let i = 0; i < shoppingList[element].length; i++) {
-        let deleteButton = $('<button>delete</button>');
-        let itemNamePrice = $(`<span>${shoppingList[element][i]}</span>`)
-        let newItemDiv = $('<div>');
-        $(newItemDiv).append(itemNamePrice).append(deleteButton);
-        $(".orderedItem").append(newItemDiv)
-        $(newItemDiv).on('click', (event) => {
-          let shoppingList = JSON.parse(window.localStorage.getItem("shoppingList"));
-          // console.log(shoppingList[element].splice(i, 1))
-          window.localStorage.setItem("shoppingList", JSON.stringify(shoppingList));
-          createItemList(shoppingList)
-        })
-        let newArray = shoppingList[element][i].split("$")
-        // console.log(newArray[newArray.length-1]) 
-        allItemLists.push(newArray[newArray.length-1])
-      }}
-
+  let total = 0;
+  $('#collapsiblelist').empty()
+  Object.keys(shoppingList).forEach((date) => {
+    let newListItem = $('<li>');
+    const dateHeader = $(`<div class="collapsible-header"><i class="material-icons">filter_drama</i>${date}</div>`)
+    $(newListItem).append(dateHeader);
+    let dateBody = $(`<div class="collapsible-body"></div>`);
+    $(newListItem).append(dateBody);
+    let newUnorderedList = $('<ul>');
+    $(dateBody).append(newUnorderedList);
+    Object.keys(shoppingList[date]).forEach((element) => {
+      if (shoppingList[date][element].length != 0) {
+        $(newUnorderedList).append(`<div>${element} : </div>`)
+        for (let i = 0; i < shoppingList[date][element].length; i++) {
+          let deleteButton = $('<button>delete</button>');
+          let itemNamePrice = $(`<span>${shoppingList[date][element][i]}</span>`)
+          let newItemDiv = $('<div>');
+          $(newItemDiv).append(itemNamePrice).append(deleteButton);
+          $(newUnorderedList).append(newItemDiv)
+          $(deleteButton).on('click', (event) => {
+            let shoppingList = JSON.parse(window.localStorage.getItem("shoppingList"));
+            console.log(shoppingList[date][element].splice(i, 1))
+            if (shoppingList[date][element].length == 0) delete shoppingList[date][element];
+            if ((Object.keys(shoppingList[date])).length == 0) delete shoppingList[date];
+            window.localStorage.setItem("shoppingList", JSON.stringify(shoppingList));
+            createItemList(shoppingList)
+          })
+          let newArray = shoppingList[date][element][i].split("$")
+          // console.log(newArray[newArray.length-1]) 
+          allItemLists.push(newArray[newArray.length - 1])
+          const itemPrice = parseFloat(newArray[newArray.length - 1]);
+          total = total + itemPrice;
+          console.log(total);
+        }
+      }
+    })
+    $('#collapsiblelist').append(newListItem)
   })
-  if(allItemLists!== null) {
+  if (allItemLists !== null) {
     $(".totalCost").empty()
-    var total = allItemLists.reduce((total, priceString)=> total + parseFloat(priceString), 0).toFixed(2);
-    $(".totalCost").append(`<p class="left-align">${total}</p>`)
-    }   
+    // var total = allItemLists.reduce((total, priceString) => total + parseFloat(priceString), 0).toFixed(2);
+    $(".totalCost").append(`<p class="left-align">${total.toFixed(2)}</p>`)
+  }
 }
 
 function save(element, date) {
-  let shoppingList = JSON.parse(window.localStorage.getItem("shoppingList"))
-  if (window.localStorage.getItem("shoppingList") === null) shoppingList = {};
   const restaurantName = $(element).parent().parent().children().first().find('span').html();
   const item = $(element).parent().children().first().html();
-  if (typeof shoppingList[restaurantName] === "undefined") shoppingList[restaurantName] = []
-  shoppingList[restaurantName].push(item)
+  let shoppingList = JSON.parse(window.localStorage.getItem("shoppingList"))
+  if (window.localStorage.getItem("shoppingList") === null) shoppingList = {};
+  if (typeof shoppingList[date] === "undefined") shoppingList[date] = {}
+  if (typeof shoppingList[date][restaurantName] === "undefined") shoppingList[date][restaurantName] = []
+  shoppingList[date][restaurantName].push(item)
   window.localStorage.setItem("shoppingList", JSON.stringify(shoppingList))
-  const itemList = shoppingList[restaurantName]
   createItemList(shoppingList)
-  console.log(date)
-  console.log(shoppingList)
-  }
+}
+
