a67fcdc4eb27b917f26ee31981aea9e907dcb9c2
Andrew Murray <atmurray@bellsouth.net>
2020-06-15 11:50:52 -0400

    heroku prep

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,8 @@
 const Sequelize = require('sequelize');
 
+if(process.env.JAWSDB_URL){
+   const sequilize = new Sequelize(process.env.JAWSDB_URL);
+}else{
 const sequelize = new Sequelize('mealTime_db', 'root', 'root', {
     host: '127.0.0.1',
     port: 3307,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,11 @@
 INSERT INTO userProfiles(first_name, last_name, userName, passWord, address)
-VALUES('Andrew', 'Murray', 'Los0013', 'icecream', '5212 Medlock Corners Dr.' );
+VALUES('Andrew', 'Murray', 'atmurray@bellsouh.net', 'icecream', '5212 Medlock Corners Dr.' );
 
 INSERT INTO userProfiles(first_name, last_name, userName, passWord, address)
-VALUES('Henry', 'Blanksman', 'theBlank', 'pizza', '1895 Peachtree Rd.');
+VALUES('Henry', 'Blanksman', 'theBlank@gmail.com', 'pizza', '1895 Peachtree Rd.');
 
 INSERT INTO orders(userName, restaurant, orders, total, orderDate)
-VALUES('Los0013', 'Little Azios', 'Cheese Pizza', 15.25, '2020-04-06');
+VALUES('atmurray@bellsouh.net', 'Little Azios', 'Cheese Pizza', 15.25, '2020-04-06');
 
 INSERT INTO orders(userName, restaurant, orders, total, orderDate)
-VALUES('theBlank', 'Maggianos', 'spaghetti', 10.99, '2020-01-21');
+VALUES('theBlank@gmail.com', 'Maggianos', 'spaghetti', 10.99, '2020-01-21');
